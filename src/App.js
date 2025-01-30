1b9fe60413c01befa16424a0cb8b90be8087e4a6
睿 <122418084+Rui01sir@users.noreply.github.com>
2025-01-30 10:55:22 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Company from './components/page/Company';
 import Contact from './components/page/Contact';
 import Product from './components/page/Product';
 import ProductList from './components/page/ProductList'; 
-// import SupportList from './components/page/SupportList';
+import SupportList from './components/page/SupportList';
 import News from './components/page/News';
 import NewsDetail from './components/page/NewsDetail';
 import Page404 from './components/page/Page404';
@@ -32,6 +32,7 @@ function App() {
         <Route path="/" element={<Loading Component={HomePage} onLoadingFinish={handleLoadingFinish} />} />
         <Route path="/Company" element={<Loading Component={Company} onLoadingFinish={handleLoadingFinish} />} />
         <Route path="/Contact" element={<Loading Component={Contact} onLoadingFinish={handleLoadingFinish} />} />
+        <Route path="/SupportList" element={<Loading Component={SupportList} onLoadingFinish={handleLoadingFinish} />} />
         <Route path="/Product" element={<Loading Component={Product} onLoadingFinish={handleLoadingFinish} />} />
         <Route path="/product/:id" element={<ProductList />} />
         <Route path="/News" element={<Loading Component={News} onLoadingFinish={handleLoadingFinish} />} />
